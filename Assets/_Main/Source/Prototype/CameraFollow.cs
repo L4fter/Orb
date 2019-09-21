5c60d024db47d78beaea535d5c5d2e509232182b
g.isachenko <george.isachenko@tatemgames.com>
2019-09-21 15:28:47 +0300

    Moving celestials

---
+++ Assets/_Main/Source/Prototype/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform followee;
+
+    private Vector3 delta;
+    
+    void Start()
+    {
+        delta = this.transform.position - followee.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = followee.transform.position + delta;
+    }
+}
---
+++ Assets/_Main/Source/Prototype/GravitatingObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class GravitatingObject : MonoBehaviour
+{
+    public const float GConstant = 0.05f;
+    
+    public Vector2 velocity;
+    public float Mass = 1;
+
+    public bool attractsOthers = false;
+    public bool isAttractedByOthers = true;
+    
+    void FixedUpdate()
+    {
+        if (!isAttractedByOthers)
+        {
+            return;
+        }
+        
+        var maxDeltaV = 1;
+        var gravitatingObjects = FindObjectsOfType<GravitatingObject>();
+        var totalDeltaV = Vector3.zero;
+
+        foreach (var celestialBody in gravitatingObjects.Where(o => o.attractsOthers).Where(o => o != this))
+        {
+            var direction = celestialBody.transform.position - this.transform.position;
+            var deltaV = direction.normalized * (GConstant * celestialBody.Mass / direction.sqrMagnitude);
+
+            totalDeltaV += deltaV;
+        }
+        
+        var totalDeltaV2d = Vector2.ClampMagnitude(new Vector2(totalDeltaV.x, totalDeltaV.y), maxDeltaV);
+
+        this.velocity += totalDeltaV2d;
+        
+        this.transform.Translate(velocity * Time.fixedDeltaTime);
+    }
+}
