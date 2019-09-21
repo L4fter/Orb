b98df52cf930275ad91b493b2d603d666abf7d5b
g.isachenko <g.isachenko@globalplaygames.com>
2019-09-22 02:13:00 +0300

    Menu with no deselect

---
+++ Assets/_Main/Source/Ui/Utils/DontAllowDeselect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DontAllowDeselect : MonoBehaviour
+{
+
+    public GameObject selectIfNothingIsSelected;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (!EventSystem.current.currentSelectedGameObject)
+        {
+            EventSystem.current.SetSelectedGameObject(selectIfNothingIsSelected);
+        }
+    }
+}
