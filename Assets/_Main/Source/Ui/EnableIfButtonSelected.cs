bf835cb22f7b3573d2dd9887a168178f99581e02
g.isachenko <g.isachenko@globalplaygames.com>
2019-09-21 23:35:35 +0300

    TMPro, main menu mockup

---
+++ Assets/_Main/Source/Ui/EnableIfButtonSelected.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class EnableIfButtonSelected : MonoBehaviour
+{
+   public Button button;
+   private TMP_Text text;
+
+   private void Awake()
+   {
+      text = GetComponent<TMP_Text>();
+   }
+
+   private void Update()
+   {
+      if (button.gameObject == EventSystem.current.currentSelectedGameObject &&
+          !this.text.enabled)
+      {
+         this.text.enabled = true;
+      }
+      
+      if (button.gameObject != EventSystem.current.currentSelectedGameObject &&
+          this.text.enabled)
+      {
+         this.text.enabled = false;
+      }
+   }
+}
